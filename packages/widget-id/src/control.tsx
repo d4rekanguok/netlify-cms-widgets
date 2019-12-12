4970bc4891b76c47cf85450451f17c89d25656ed
gurkodil <me@gustafwennerstrom.com>
2019-12-12 14:16:55 +0100

    regenerate id if component updates with undefined value

--- packages/widget-id/src/control.tsx
+++ packages/widget-id/src/control.tsx
@@ -1,25 +1,33 @@
-import * as React from 'react'
-import shortid from 'shortid'
-import { WidgetProps } from '@ncwidgets/common-typings'
+import * as React from "react"
+import shortid from "shortid"
+import { WidgetProps } from "@ncwidgets/common-typings"
 
 export class Control extends React.Component<WidgetProps> {
-  public componentDidMount() {
-    const {
-      value,
-      field,
-      onChange,
-    } = this.props
-    
-    if (value) return
-  
-    const usePrefix = field.get('prefix')
-    const useTimestamp = field.get('timestamp')
+  constructor(props: WidgetProps) {
+    super(props)
 
-    const prefix = usePrefix ? usePrefix + '-' : ''
-    const timestamp = useTimestamp ? Date.now() + '-' : ''
+    if (props.value) return
+
+    this.generateId()
+  }
+
+  generateId() {
+    const { field, onChange } = this.props
+    const usePrefix = field.get("prefix")
+    const useTimestamp = field.get("timestamp")
+
+    const prefix = usePrefix ? usePrefix + "-" : ""
+    const timestamp = useTimestamp ? Date.now() + "-" : ""
 
     const id = prefix + timestamp + shortid()
+
     onChange(id)
+  }
+
+  componentDidUpdate() {
+    if (this.props.value) return
+
+    this.generateId()
   }
 
   public render() {
@@ -28,16 +36,16 @@ export class Control extends React.Component<WidgetProps> {
       classNameWrapper,
       setActiveStyle,
       setInactiveStyle,
-      value,
+      value
     } = this.props
     return (
       <input
         type="text"
         className={classNameWrapper}
         style={{
-          color: '#cdcdcd',
+          color: "#cdcdcd"
         }}
-        value={value || ''}
+        value={value || ""}
         id={forID}
         onFocus={setActiveStyle}
         onBlur={setInactiveStyle}
